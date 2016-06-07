68e88697bf4b9f94e218183478330bddeb7617a4
Lossy Redgement <cindy.zyd@126.com>
2016-06-07 16:16:48 +0800

    number of songs of an artist

---
+++ data/src/sum.py
@@ -0,0 +1,30 @@
+#
+#   Extract daily information for every artist.
+#   
+#   Structure:
+#   
+#   artist_id,song_id,play/download/collect
+#   Order by ds, count(desc)                    
+#
+
+import MySQLdb, config.config as cfg
+
+sql_sum = "select Songs.artist_id as id,count(*) as sum from Songs group by Songs.artist_id;"
+
+print "Connecting to the database..."
+conn = MySQLdb.connect ( host = cfg.HOST, user = cfg.USER,\
+                        passwd = cfg.PASSWORD,\
+                         charset='utf8')
+print "Connected."
+# Get cursor
+cur = conn.cursor(MySQLdb.cursors.DictCursor)
+conn.select_db('tianchi_music')
+
+print "Start executing sql_highest_play"
+cur.execute(sql_sum)
+play = open(cfg.ROOT + '/data/derived/singers/artist_sum.txt', 'w') 
+dates = []
+for row in cur.fetchall():
+    play.write(row['id']+'\t'+str(row['sum'])+'\n')
+
+
